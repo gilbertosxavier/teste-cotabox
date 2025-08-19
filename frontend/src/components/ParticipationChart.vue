3dbee9d5f5b3f9e6f8c83ed01b0f2e3b930f48c5
Gilberto Xavier <93664041+gilbertosxavier@users.noreply.github.com>
2025-08-19 15:34:48 -0300

    feat: Customizado gráfico.

--- frontend/src/components/ParticipationChart.vue
+++ frontend/src/components/ParticipationChart.vue
@@ -1,7 +1,28 @@
 <template>
-  <div class="chart-container">
-    <canvas ref="chartCanvas"></canvas>
-  </div>
+  <section class="chart-wrapper" >
+    
+    <div class="chart-container">
+      <canvas ref="chartCanvas"></canvas>
+    </div>
+
+  
+    <div class="legend">
+      <ul>
+        <li v-for="(participant, index) in participants" :key="participant.id"
+        :style="{color: colors[index % colors.length] }"
+        
+        >
+          <span
+            class="color-box"
+            :style="{backgroundColor: colors[index % colors.length] }"
+          ></span>
+
+          {{ participant.first_name }} {{ participant.last_name }} - {{ participant.participation }} %
+        </li>
+      </ul>
+    </div>
+
+  </section>
 </template>
 
 <script>
@@ -9,6 +30,12 @@ import Chart from 'chart.js/auto';
 
 export default {
   props: ['participants'],
+  data(){
+    return {
+      chart: null,
+      colors: ['#2d97de', '#16ba9a', '#e94a35', '#9c56b8', '#bdc3c7' ]
+    };
+  },
   watch: {
     participants: {
       handler: 'renderChart',
@@ -21,19 +48,20 @@ export default {
   methods: {
     renderChart() {
       if (this.chart) this.chart.destroy();
-      const ctx = this.$refs.chartCanvas.getContext('2d');
-      const labels = this.participants.map(participant => `${participant.first_name} ${participant.last_name}`);
-      const data = this.participants.map(p => p.participation);
-      this.chart = new Chart(ctx, {
+      const context = this.$refs.chartCanvas.getContext('2d');
+      const data = this.participants.map(percent => percent.participation);
+      this.chart = new Chart(context, {
         type: 'doughnut',
         data: {
-          labels,
           datasets: [{ 
             data,
-            backgroundColor: ['#36A2EB','#00C853','#9C27B0','#9E9E9E','#FF5252']
+            backgroundColor: this.colors
           }]
         },
-        options: { responsive: true, maintainAspectRatio: false }
+        options: {
+           responsive: true, 
+           maintainAspectRatio: false,
+          }
       });
     }
   }
@@ -41,11 +69,35 @@ export default {
 </script>
 
 <style>
+.chart-wrapper{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 30px;
+}
+
 .chart-container {
-  width: 400px;
-  height: 400px;
-  max-width: 100%;
-  margin: 0 auto;
+  width: 300px;
+  height: 300px;
+}
+
+.legend ul {
+  list-style: none;
+}
+
+.legend li {
+  display: flex;
+  align-items: center;
+  margin-bottom: 8px;
+  font-size: 16px;
+  font-weight: bold;
+}
+
+.color-box {
+  width: 26px;
+  height: 26px;
+  border-radius: 6px;
+  margin-right: 8px;
 }
 
 </style>
