75d57ac0028598bbfdbcd9c1b629b292e983d63e
Gilberto Xavier <93664041+gilbertosxavier@users.noreply.github.com>
2025-08-19 16:58:57 -0300

    style:  Ajustado estilos da tabela.

--- frontend/src/components/ParticipantsTable.vue
+++ frontend/src/components/ParticipantsTable.vue
@@ -1,19 +1,25 @@
 <template>
   <table class="data-table">
+    <colgroup>
+      <col class="number">
+      <col class="first-name">
+      <col class="last-name">
+      <col class="participation">
+    </colgroup>
     <thead>
       <tr>
-        <th>#</th>
+        <th></th>
         <th>First name</th>
         <th>Last name</th>
         <th>Participation</th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(p, index) in participants" :key="p._id">
+      <tr v-for="(participant, index) in participants" :key="participant._id">
         <td>{{ index + 1 }}</td>
-        <td>{{ p.first_name }}</td>
-        <td>{{ p.last_name }}</td>
-        <td>{{ p.participation }}%</td>
+        <td>{{ participant.first_name }}</td>
+        <td>{{ participant.last_name }}</td>
+        <td>{{ participant.participation }}%</td>
       </tr>
     </tbody>
   </table>
@@ -24,3 +30,39 @@ export default {
   props: ['participants']
 }
 </script>
+
+
+<style>
+
+table, th, td {
+  border: 1px solid #444;
+  border-collapse: collapse;
+  padding: 8px;
+}
+
+thead > tr {
+  text-align:left;
+  font-size: 14px;
+}
+
+tbody{
+  font-size: 12px;
+}
+
+td:first-child, th:first-child, td:last-child, th:last-child {
+  text-align: center;
+}
+
+.number{
+  width: 30px;
+}
+
+.first-name, .last-name {
+  width: 200px;
+}
+
+.participation {
+  width: 150px;
+}
+
+</style>
