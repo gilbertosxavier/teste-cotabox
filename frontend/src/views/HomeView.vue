a5c08eeab67c6dcc6e93a42ae55a893cf12ef026
Gilberto Xavier <93664041+gilbertosxavier@users.noreply.github.com>
2025-08-21 00:07:34 -0300

    style: adicionado spinner para tela enquanto dados são carregados.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -9,10 +9,20 @@
         <h1>DATA</h1>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
       </section>
-
+      
       <section class="chart">
-        <ParticipantsTable :participants="participants"/>
-        <ParticipationChart :participants="participants"/>
+        <div  v-if="loading" class="spinner-container">
+          <img src="../assets/spinner.svg" alt="Loading...">
+          <p>Loading...</p>
+        </div>
+        
+        <div v-else-if="participants.length === 0">
+          <p class="no-data">You do not have any registered data yet.</p>
+        </div>
+        <template v-else>
+          <ParticipantsTable :participants="participants"/>
+          <ParticipationChart :participants="participants"/>
+        </template>
       </section>
     </main>
     
@@ -24,12 +34,14 @@ import api from '../services/api';
 import ParticipantForm from '../components/ParticipantForm.vue';
 import ParticipantsTable from '../components/ParticipantsTable.vue';
 import ParticipationChart from '../components/ParticipationChart.vue';
+import spinner from '../assets/spinner.svg';
 
 export default {
   components: { ParticipantForm, ParticipantsTable, ParticipationChart },
   data() {
     return {
-      participants: []
+      participants: [],
+      loading: true,
     }
   },
   mounted() {
@@ -37,11 +49,14 @@ export default {
   },
   methods: {
     async fetchParticipants() {
+      this.loading = true;
       try {
         const res = await api.get('/participants');
         this.participants = res.data;
       } catch (err) {
         console.error(err);
+      } finally{
+        this.loading = false;
       }
     },
     async addParticipant(participant) {
@@ -82,11 +97,35 @@ main{
   width: 100%;
   max-width: 1080px;
   display: flex;
+  flex-direction: row;
   align-items: center;
   justify-content: space-around;
   margin: 16px;
   padding: 18px;
   gap: 18px;
+}
+
+.spinner-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+}
+
+.spinner-container img {
+  width: 70px;
+  height: 70px;
+}
+
+.spinner-container p {
+  font-weight: bold;
+  font-size: 26px;
+}
+
+.no-data {
+  font-weight: bold;
+  font-size: 18px;
 }
 
 @media (max-width: 890px) {
