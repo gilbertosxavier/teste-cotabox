95a331036a429837497f4612a13c698607124131
Gilberto Xavier <93664041+gilbertosxavier@users.noreply.github.com>
2025-08-22 00:13:36 -0300

    style: Criado estilo para componentes do modal.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -31,10 +31,14 @@
     <div v-if="showModal" class="modal-backdrop" >
       <div class="modal">
         <h2>Edit participation</h2>
-        <input type="number" v-model="editedParticipation">
-        <div class="buttons">
-          <button @click="saveChanges" >Save</button>
-          <button @click="showModal = false">Cancel</button>
+        <p>Editando a participação de {{ participantToEdit.first_name }} {{ participantToEdit.last_name }}</p>
+        <div class="input-container">
+          <span>Porcentagem:</span>
+          <input type="number" v-model="editedParticipation">
+        </div>
+        <div class="buttons-container">
+          <button class="button button--cancel" @click="showModal = false">Cancel</button>
+          <button class="button button--save" @click="saveChanges" >Save</button>
         </div>
       </div>
     </div>
@@ -182,7 +186,7 @@ main{
   max-width: 1280px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
   margin: 16px;
   padding: 18px;
   gap: 50px;
@@ -214,7 +218,7 @@ main{
 .modal-backdrop{
   position: fixed;
   inset: 0; 
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: #f8f9facb;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -222,18 +226,85 @@ main{
 }
 
 .modal {
+  box-shadow: 0 2px 18px rgba(0,0,0,0.6) ;
   width: 400px;
-  height: 400px;
+  height: 300px;
   display: flex;
   flex-direction: column;
   align-items: center ;
-  justify-content: center;
+  justify-content: space-around;
   gap: 16px;
   background: #fff;
-  padding: 16px;
+  padding: 18px;
   border-radius: 6px;
 
 }
+
+.modal h2{
+  font-weight: bold;
+  color: #212529
+}
+
+.modal p {
+  font-size: 14px;
+  color: #4e4e4e;
+  font-weight: semibold;
+}
+
+.input-container{
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.input-container span{
+  font-size: 16px;
+  color: #4e4e4e;
+  font-weight: bold;
+}
+
+.input-container input {
+  width: 100%;
+  height: 40px;
+  background-color: #F8F9FA;
+  outline: none;
+  border: 1px solid #E0E0E0;
+  border-radius: 4px;
+  padding: 8px;
+  font-size: 16px;
+  color: #4e4e4e;
+  font-weight: bold;
+}
+
+.buttons-container{
+  width: 100%;
+  display:flex;
+  gap: 16px;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.button {
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  padding: 10px 40px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+
+
+.button--save{
+  background-color: #28A745;
+}
+
+.button--cancel{
+  background-color: #8b99a7;
+}
+
 
 @media (max-width: 1000px) {
   .chart {
